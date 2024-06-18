42377f03668fd6ab4d5e76ee6a1e4267591c54b8
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-06-18 12:10:07 -0300

    Implementing App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 import UdemyCourse from './components/UdemyCourse';
 
 function App() {
-
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
+        <a className="navbar-brand" href="/">
+          <img src=".images/udemy.png" alt="Udemy logo" />
+        </a>
+        <h1 className="course-title">
+          | Curso de React.js Completo
+        </h1>
+      </nav>
+      <main className="main-content">
+        <UdemyCourse />
+      </main>
+    </div>
+  );
 }
 
 export default App;
